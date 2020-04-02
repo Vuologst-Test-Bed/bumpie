de46a7a18a99fe25e4742e6d478000b2faee9677
Heather <dylanchoum123@yahoo.com>
2020-04-01 23:56:23 -0700

    removed flexbox and added grid for non expanding columns, made 3 equal sized columns and justified items to be center.

--- client-react/src/common/Header.js
+++ client-react/src/common/Header.js
@@ -5,13 +5,15 @@ import logo from "../assets/logo.png";
 import user_icon from "../assets/user_icon.png";
 
 const Container = styled.header`
-  display: flex;
-  justify-content: space-around;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
   align-items: center;
   background-color: #8fe8df;
   color: #ff4d4d;
   padding-top: 20px;
   padding-bottom: 20px;
+  justify-items: center;
 `;
 const Link = styled.a`
   font-size: 25px;
