027d75e286425c8d0f14a392c7b47b7a6a9865e2
Anthony Vu <tk42111@gmail.com>
2020-04-01 22:41:25 -0700

    create basic header layout

--- client-react/src/common/Header.js
+++ client-react/src/common/Header.js
@@ -1,9 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+import logo from "../assets/logo.png";
+import user_icon from "../assets/user_icon.png";
+
+const Container = styled.header`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #8fe8df;
+  color: #ff4d4d;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+const Link = styled.a`
+  font-size: 25px;
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+`;
 
 const Header = () => (
-  <div>
-    <h1>Header</h1>
-  </div>
+  <Container>
+    <Link href="#">DASHBOARD</Link>
+    <img src={logo} style={{ height: 50 }} alt="logo" />
+    <img src={user_icon} style={{ height: 50 }} alt="user icon" />
+  </Container>
 );
 
 export default Header;
