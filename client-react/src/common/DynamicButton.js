a9fd155e01a69451f4348e1e366d5b60cc80acc2
Heather <dylanchoum123@yahoo.com>
2020-04-02 21:44:50 -0700

    enhancement: remove class and added in dynamic styling inside const

--- client-react/src/common/DynamicButton.js
+++ client-react/src/common/DynamicButton.js
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import styled from "styled-components";
 import Button from 'react-bootstrap/Button';
 
-class DynamicButton extends Component {
 
-  render() {
-      const {variant, onclick, color, text} = this.props;
-      const styles = {
-        buttonText: {
-          color: color,
-          textDecoration: "none",
-          textTransform: "upperCase"
-        }
-      }
 
-      return (
-        <Button variant={variant} onClick={onclick} style={styles.buttonText}>{text}</Button>
-      );
+
+const DynamicButton = (props) => {
+  let textColor = {
+    color: props.color
   }
-}
 
+  let basicStyle = {
+    textDecoration: "none",
+    textTransform: "upperCase"
+  }
+  return (
+    <Button variant={props.variant} onClick={props.onclick} style={{...basicStyle,...textColor
+    }}>{props.text}</Button>
+  );
+};
 
 export default DynamicButton;
