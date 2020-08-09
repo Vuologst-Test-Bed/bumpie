e0a2eba560ccae8ddb0953c0b279fe16b63943f9
Heather <dylanchoum123@yahoo.com>
2020-08-09 13:30:09 -0700

    enhance: added sign in page route

--- client-react/src/Routes.js
+++ client-react/src/Routes.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import NotificationSettings from "./notification-settings";
+import SignIn from "./signin";
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/notification-settings">
         <NotificationSettings />
+      </Route>
+      <Route exact path="/sign-in">
+        <SignIn />
       </Route>
     </Switch>
   );
