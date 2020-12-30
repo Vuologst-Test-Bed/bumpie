ff284bcc00c73eab912d280df85d911788cfae6d
Anthony Vu <tk42111@gmail.com>
2020-12-30 10:49:23 -0800

    update aws cognito endpoint

--- client-react/src/config.js
+++ client-react/src/config.js
@@ -1,18 +1,18 @@
 const dev = {
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_W9xUtvYng",
-    APP_CLIENT_ID: "6veevn4el3p4bh3qgjeaug5ok2",
-    IDENTITY_POOL_ID: "us-east-1:fb6affaf-249a-4c81-b364-c3889e8530f3",
+    USER_POOL_ID: "us-east-1_mhqAnffLQ",
+    APP_CLIENT_ID: "2u8n8nfi19hlus4o0rolcaoptb",
+    IDENTITY_POOL_ID: "us-east-1:3e765d63-9a42-42c4-ab1b-b4ec214cc902",
   },
 };
 
 const prod = {
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_W9xUtvYng",
-    APP_CLIENT_ID: "6veevn4el3p4bh3qgjeaug5ok2",
-    IDENTITY_POOL_ID: "us-east-1:fb6affaf-249a-4c81-b364-c3889e8530f3",
+    USER_POOL_ID: "us-east-1_mhqAnffLQ",
+    APP_CLIENT_ID: "2u8n8nfi19hlus4o0rolcaoptb",
+    IDENTITY_POOL_ID: "us-east-1:3e765d63-9a42-42c4-ab1b-b4ec214cc902",
   },
 };
 
