02d4ee1f02a64a4e3c8fdb1d1912514e4200bb8c
Heather Tran <dylanchoum123@yahoo.com>
2021-01-26 21:14:20 -0800

    Swirls landing (#202)
    * swirls
    
    * swirls size update
    
    * switch to higher quality svg for background
    
    * size adjustment

--- client-react/src/home/index.js
+++ client-react/src/home/index.js
@@ -8,11 +8,8 @@ import { ReactComponent as DrawingIllustration } from "../assets/DrawingBikeIllu
 import { ReactComponent as PostItDrawing } from "../assets/PostItDrawing.svg";
 import { ReactComponent as ScienceIllustration } from "../assets/ScienceIllustration.svg";
 import { ReactComponent as CoffeeMidSmoke } from "../assets/CoffeeMidSmoke.svg";
-
-import CoffeeSwirls from "../assets/CoffeeSwirls.svg";
-import PostItSwirl from "../assets/PostItSwirl.svg";
 import LandingMiddleBackground from "../assets/LandingMiddleBackground.svg";
-import ClipboardSwirls from "../assets/clipboard_swirls.svg";
+import AllSwirls from "../assets/allswirlslarge.svg";
 
 const HomePageH1 = styled.h1`
   font-family: Quicksand;
@@ -53,26 +50,16 @@ const ClipBoardSection = styled.section`
   align-items: center;
   height: 1000px;
   overflow: hidden;
-  background-image: url(${ClipboardSwirls});
-  background-size: 120%;
+  margin-bottom: 550px;
 `;
 
 const BikeSection = styled.section`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-image: url(${PostItSwirl});
-  background-position: left center;
-  background-size: 120%;
-  background-repeat: no-repeat;
-  min-height: 1500px;
 `;
 
 const PostItDiv = styled.div`
-  background-image: url(${LandingMiddleBackground});
-  background-position: left top;
-  background-size: 2000px;
-  background-repeat: no-repeat;
   min-height: 1500px;
 `;
 
@@ -81,10 +68,6 @@ const CoffeeSection = styled.section`
   justify-content: space-between;
   align-items: center;
   max-height: 1000px;
-  background-image: url(${CoffeeSwirls});
-  background-size: 140%;
-  background-position: center top;
-  background-repeat: no-repeat;
   overflow: "hidden";
 `;
 
@@ -93,75 +76,92 @@ const TextDiv = styled.div`
   margin-right: 100px;
 `;
 
+const SwirlsDiv = styled.div`
+  background-image: url(${AllSwirls});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: 2250px;
+`;
+
+const StyledMain = styled.main`
+  background-image: url(${LandingMiddleBackground});
+  background-position: left center;
+  background-size: 2100px;
+  background-repeat: no-repeat;
+`;
+
 const Home = () => (
-  <main style={{ backgroundColor: "#8fe8df", overflowX: "hidden" }}>
-    <Navbar />
-    <ClipBoardSection>
-      <TextDiv>
-        <HomePageH1>
-          Track your progress. <br />
-          Improve your life.
-        </HomePageH1>
-        <HomePageHeroP>
-          Visualize and track your strengths. Build your stats.
-        </HomePageHeroP>
-        <StartBtn>Get Started</StartBtn>
-      </TextDiv>
-      <DrawingIllustration
-        style={{
-          position: "relative",
-          height: "900px",
-          right: -300,
-        }}
-      />
-    </ClipBoardSection>
-    <PostItDiv>
-      <BikeSection>
-        <PostItDrawing
+  <StyledMain style={{ backgroundColor: "#8fe8df", overflowX: "hidden" }}>
+    <SwirlsDiv>
+      <Navbar />
+      <ClipBoardSection>
+        <TextDiv>
+          <HomePageH1>
+            Track your progress. <br />
+            Improve your life.
+          </HomePageH1>
+          <HomePageHeroP>
+            Visualize and track your strengths. Build your stats.
+          </HomePageHeroP>
+          <StartBtn>Get Started</StartBtn>
+        </TextDiv>
+        <DrawingIllustration
           style={{
             position: "relative",
-            left: 0,
-            width: 2800,
+            height: "900px",
+            right: -300,
           }}
         />
+      </ClipBoardSection>
+      <PostItDiv>
+        <BikeSection>
+          <PostItDrawing
+            style={{
+              position: "relative",
+              left: 0,
+              width: 2800,
+            }}
+          />
+          <TextDiv>
+            <HomePageH2 white>Bump up your life with Bumpie!</HomePageH2>
+            <HomePageHeroP white>
+              Bumpie allows you to track your life skills through five
+              categories. Each category has sub-categories that allows you to
+              track your advancements! Bump up your life and progress yourself
+              today!
+            </HomePageHeroP>
+          </TextDiv>
+        </BikeSection>
+      </PostItDiv>
+      <CoffeeSection>
         <TextDiv>
-          <HomePageH2 white>Bump up your life with Bumpie!</HomePageH2>
-          <HomePageHeroP white>
-            Bumpie allows you to track your life skills through five categories.
-            Each category has sub-categories that allows you to track your
-            advancements! Bump up your life and progress yourself today!
+          <HomePageH2>Personalize your categories.</HomePageH2>
+          <HomePageHeroP>
+            Whether you are trying to build your fitness goals or improve your
+            culinary skills, Bumpie allows you to personalize your categories
+            based on your own life. Your stats will increase and/or decrease
+            over time, allowing you to visualize your own progress!
           </HomePageHeroP>
         </TextDiv>
-      </BikeSection>
-    </PostItDiv>
-    <CoffeeSection>
-      <TextDiv>
-        <HomePageH2>Personalize your categories.</HomePageH2>
-        <HomePageHeroP>
-          Whether you are trying to build your fitness goals or improve your
-          culinary skills, Bumpie allows you to personalize your categories
-          based on your own life. Your stats will increase and/or decrease over
-          time, allowing you to visualize your own progress!
-        </HomePageHeroP>
-      </TextDiv>
-      <div style={{ alignSelf: "flex-end" }}>
-        <ScienceIllustration
-          style={{
-            width: 980,
-          }}
-        />
-        <CoffeeMidSmoke
-          style={{
-            position: "relative",
-            width: 195,
-            top: -1173,
-            left: 604,
-          }}
-        />
-      </div>
-    </CoffeeSection>
-    <Footer green />
-  </main>
+        <div style={{ alignSelf: "flex-end" }}>
+          <ScienceIllustration
+            style={{
+              width: 980,
+            }}
+          />
+          <CoffeeMidSmoke
+            style={{
+              position: "relative",
+              width: 195,
+              top: -1173,
+              left: 604,
+            }}
+          />
+        </div>
+      </CoffeeSection>
+      <Footer green />
+    </SwirlsDiv>
+  </StyledMain>
 );
 
 export default Home;
