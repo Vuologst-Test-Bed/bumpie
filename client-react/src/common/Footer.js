1878ceb66de4636fd10797852861fdf224dc7b44
Heather Tran <dylanchoum123@yahoo.com>
2021-02-16 20:48:45 -0800

    footer updates (#220)

--- client-react/src/common/Footer.js
+++ client-react/src/common/Footer.js
@@ -1,62 +1,41 @@
 import React from "react";
 import styled from "styled-components";
-import { Route, Switch } from "react-router-dom";
-
-const handleBgColor = (color) => {
-  switch (color) {
-    case "green":
-      return "#46A3A5";
-    case "yellow":
-      return "#ffd217";
-    default:
-      return "#fff";
-  }
-};
-
-const handleColor = (color) => {
-  switch (color) {
-    case "green":
-      return "#8FE8DF";
-    case "yellow":
-      return "#ff4d4d";
-    default:
-      return "#C5C5C4";
-  }
-};
+import { Link } from "react-router-dom";
 
 const Container = styled.footer`
-  background-color: ${(props) => handleBgColor(props.bg)};
+  background-color: #fff;
   text-align: center;
-  color: ${(props) => handleColor(props.bg)};
+  color: #c5c5c4;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   flex-shrink: 0;
+  padding-left: 100px;
+  padding-right: 100px;
   width: 100%;
   min-height: 10vh;
   /* border-top: ; */
 `;
 
+const StyledLink = styled(Link)`
+  color: #c5c5c4;
+`;
+
+const StyledSpan = styled.span`
+  padding-left: 5px;
+  padding-right: 5px;
+`;
+
 const Footer = () => {
   return (
-    <Switch>
-      <Route exact path="/sign-in"></Route>
-      <Route exact path="/">
-        <Container bg="green">
-          © {new Date().getFullYear()} BUMPIE. All Rights Reserved.
-        </Container>
-      </Route>
-      <Route exact path={["/about-us", "/faq"]}>
-        <Container bg="white">
-          © {new Date().getFullYear()} BUMPIE. All Rights Reserved.
-        </Container>
-      </Route>
-      <Route>
-        <Container bg="yellow">
-          © {new Date().getFullYear()} BUMPIE. All Rights Reserved.
-        </Container>
-      </Route>
-    </Switch>
+    <Container bg="white">
+      <span>© {new Date().getFullYear()} BUMPIE. All Rights Reserved.</span>
+      <div>
+        <StyledLink to="/">Privacy Policy</StyledLink>
+        <StyledSpan>|</StyledSpan>
+        <StyledLink to="/FAQ">FAQ</StyledLink>
+      </div>
+    </Container>
   );
 };
 
